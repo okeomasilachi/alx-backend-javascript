e0d52ddffe2c4b5d691e3dfa503a01825638fccb
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-05 01:42:02 +0000

    Write a function named createIteratorObject, that will take into argument a report Object created with the previous function createReportObject.
    This function will return an iterator to go through every employee in every department.

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,26 @@
+export default function createIteratorObject(report) {
+    const departments = Object.values(report.allEmployees);
+    let currentDepartmentIndex = 0;
+    let currentEmployeeIndex = 0;
+
+    return {
+        next() {
+            if (currentDepartmentIndex >= departments.length) {
+                return { done: true };
+            }
+
+            const department = departments[currentDepartmentIndex];
+            const employee = department[currentEmployeeIndex++];
+
+            if (currentEmployeeIndex >= department.length) {
+                currentDepartmentIndex++;
+                currentEmployeeIndex = 0;
+            }
+
+            return { value: employee, done: false };
+        },
+        [Symbol.iterator]() {
+            return this;
+        }
+    };
+}
