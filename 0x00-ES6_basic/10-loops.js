2150713857b5f9b6597e0eec46934963e1994eb2
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-05 12:54:32 +0000

    Fix loop variable declaration

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
 
   /* eslint-disable-next-line no-unused-vars */
-  for (const value of array.entries()) {
+  for (value of array.entries()) {
     newArray.push(appendString + value);
   }
 
