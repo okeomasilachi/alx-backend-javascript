753ba819406f580569430b0f9f44bd894c8cabe8
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-05 13:00:15 +0000

    Refactor loop in appendToEachArrayValue function

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
 
-  /* eslint-disable-next-line no-unused-vars */
-  for (const value of array.entries()) {
+  for (const value of array) {
     newArray.push(appendString + value);
   }
 
