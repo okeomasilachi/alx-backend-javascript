c57574c9a7ff2c17069f3d24202ef271364de7c7
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-05 02:28:02 +0000

    Fix block-scoped variable assignment

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,10 @@
 export default function taskBlock(trueOrFalse) {
+  let task;
+  let task2;
+
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
 
   task = false;
