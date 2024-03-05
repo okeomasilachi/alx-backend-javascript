9d45ed6d88620a66bcf73402ab8060a6258635c3
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-05 02:24:27 +0000

    Refactor block-scoped variables to use let instead of var

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
 export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   task = false;
