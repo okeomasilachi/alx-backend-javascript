19764b47896c4ecec80355eddc2845e520e50d40
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-06 13:31:52 +0000

    Refactor handleProfileSignup function to destructure Promise.all response

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,11 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    /* eslint-disable-next-line no-unused-vars */
+    .catch((error) => {
+      console.log('Signup system offline');
+    });
 }
