d9a69049b46ebb749756f1ebd025ec10e82e7795
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-06 10:21:17 +0000

    write and export function that Returns a Promise using this prototype function getResponseFromAPI()

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  /* eslint-disable-next-line no-unused-vars */
+  return new Promise((resolve, reject) => {
+    // Your code here
+  });
+}
+
+export default getResponseFromAPI;
