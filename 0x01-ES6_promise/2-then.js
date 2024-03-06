f3917dc958576263b940473eef0041dfe5384112
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-06 10:52:17 +0000

    Refactor handleResponseFromAPI function in 0x01-ES6_promise/2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,15 @@
 function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'Success' };
-  }).catch(() => new Error());
+  promise
+    /* eslint-disable-next-line no-unused-vars */
+    .then((response) => {
+      console.log('Got a response from the API'); // Log for each resolution
+      return { status: 200, body: 'success' };
+    })
+    /* eslint-disable-next-line no-unused-vars */
+    .catch((error) => {
+      console.log('Got a response from the API'); // Log for each resolution
+      return new Error(); // Return an empty Error object
+    });
 }
 
 export default handleResponseFromAPI;
