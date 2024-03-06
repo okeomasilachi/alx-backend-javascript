bb0baa8875bcbeb6ebd28d23f0832c2432335446
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-06 13:16:02 +0000

    Refactor handleResponseFromAPI function to handle null promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,14 @@
 function handleResponseFromAPI(promise) {
-  promise
-    /* eslint-disable-next-line no-unused-vars */
-    .then((response) => {
-      console.log('Got a response from the API'); // Log for each resolution
-      return { status: 200, body: 'success' };
-    })
-    /* eslint-disable-next-line no-unused-vars */
-    .catch((error) => new Error());
+  if (promise) {
+    promise
+      /* eslint-disable-next-line no-unused-vars */
+      .then((response) => {
+        console.log('Got a response from the API'); // Log for each resolution
+        return { status: 200, body: 'success' };
+      })
+      /* eslint-disable-next-line no-unused-vars */
+      .catch((error) => new Error());
+  }
 }
 
 export default handleResponseFromAPI;
--- 0x01-ES6_promise/main.js
+++ 0x01-ES6_promise/main.js
@@ -1,4 +1,4 @@
 import handleResponseFromAPI from "./2-then";
 
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+const promise2 = Promise.reject();
+handleResponseFromAPI(promise2);
