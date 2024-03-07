51f313fdee4fdea972b58a8f188babfd678ed562
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-07 00:43:28 +0000

    Refactor code to use handleProfileSignup function

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: (result.status === 'fulfilled') ? result.value : result.reason,
+  }));
+}
+
+export default handleProfileSignup;
