b18b687c6e00674cc20b4e0d7964133f7cbb4c66
okeomasilachi onyedibia <110942635+Okeomasilchi@users.noreply.github.com>
2024-03-07 15:23:33 +0000

    Refactor input validation in HolbertonCourse constructor

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,20 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name === 'string') {
-      this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length === 'number') {
-      this._length = length;
-    } else {
-      throw new TypeError('Length must be a number');
-    }
-    if (Array.isArray(students)) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array');
-    }
+    this._name = typeof name === 'string' ? name : (() => { throw new TypeError('Name must be a string'); })();
+    this._length = typeof length === 'number' ? length : (() => { throw new TypeError('Length must be a number'); })();
+    this._students = Array.isArray(students) ? students : (() => { throw new TypeError('Students must be an array'); })();
   }
 
   get name() {
