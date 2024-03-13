c1dbe9094f78c85f007986c0b9c975bb177a9e59
okeomasilchi <okeomasilachi@gmail.com>
2024-03-13 02:45:11 +0100

    Refactor hasValuesFromArray function to use arrow function and every method

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
 export default function hasValuesFromArray(set, arr) {
-  for (const element of arr) {
-    if (set.has(element)) {
-      return true;
-    }
-  }
-  return false;
+  return arr.every(element => set.has(element)) ? true : false;
 }
