d4dab891855c9c22da39400a072ad85cbea044e7
okeomasilchi <okeomasilachi@gmail.com>
2024-03-13 22:26:43 +0100

    Add Student class implementation

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,3 +20,30 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
   const fullName = `${firstLetter}. ${lastName}`;
   return fullName;
 }
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class Student implements StudentClass {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
