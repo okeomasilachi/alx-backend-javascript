152d40a532bd280acd5c8b44d9f65df2fdb37d96
okeomasilchi <okeomasilachi@gmail.com>
2024-03-13 03:35:42 +0100

    Add endpoint tracking to queryAPI function

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+
+  if (count >= 4) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { queryAPI, weakMap };
