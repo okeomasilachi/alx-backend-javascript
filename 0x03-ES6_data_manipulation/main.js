402f4cd001d9d8497e9aadfd01a712018bcb4489
okeomasilchi <okeomasilachi@gmail.com>
2024-03-13 02:32:11 +0100

    Add setFromArray function and update main.js with hasValuesFromArray calls

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,3 +1,5 @@
-import createInt8TypedArray from "./5-typed_arrays.js";
+import hasValuesFromArray from "./7-has_array_values.js";
 
-console.log(createInt8TypedArray(10, 2, 89));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
