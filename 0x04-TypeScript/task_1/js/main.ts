76a8658908469f7af6349a65da589fafcad3f00f
okeomasilchi <okeomasilachi@gmail.com>
2024-03-13 22:18:30 +0100

    Delete TypeScript task files

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const fullName = `${firstLetter}. ${lastName}`;
+  return fullName;
+}
