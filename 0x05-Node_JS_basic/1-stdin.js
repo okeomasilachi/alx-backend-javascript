5658a92c159caad8ef979a43f2216ae8737f2d64
okeomasilchi <okeomasilachi@gmail.com>
2024-04-30 20:26:14 +0100

    Refactor user input handling in 1-stdin.js and add HTTP server in 4-http.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,28 +1,41 @@
-const readline = require('readline');
+// const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-// Display initial prompt
+// // Display initial prompt
+// console.log('Welcome to Holberton School, what is your name?');
+
+// // Read user input
+// rl.on('line', (input) => {
+//   if (input.trim() === '') {
+//     rl.close(); // Close the interface if input is empty
+//   } else {
+//     // Display user's name
+//     console.log(`Your name is: ${input}`);
+//     rl.close();
+//   }
+// });
+
+// // Handle program termination if input stream is not a TTY
+// if (!process.stdin.isTTY) {
+//   rl.on('close', () => {
+//     console.log('This important software is now closing');
+//     process.exit(0);
+//   });
+// }
+
+process.stdin.setEncoding('utf8');
+
 console.log('Welcome to Holberton School, what is your name?');
 
-// Read user input
-rl.on('line', (input) => {
-  if (input.trim() === '') {
-    rl.close(); // Close the interface if input is empty
-  } else {
-    // Display user's name
-    console.log(`Your name is: ${input}`);
-    rl.close();
-  }
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
 });
 
-// Handle program termination if input stream is not a TTY
-if (!process.stdin.isTTY) {
-  rl.on('close', () => {
-    console.log('This important software is now closing');
-    process.exit(0);
-  });
-}
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
