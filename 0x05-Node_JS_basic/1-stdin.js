612453e450b5ad8a898eb5106be37bff8da67beb
okeomasilchi <okeomasilachi@gmail.com>
2024-04-30 20:01:04 +0100

    Refactor user input handling in 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,26 +1,28 @@
-/*
-Create a program named 1-stdin.js that will be executed through
-command line:
-
-It should display the message Welcome to Holberton School, what is
-your name? (followed by a new line)
-The user should be able to input their name on a new line
-The program should display Your name is: INPUT
-When the user ends the program, it should display This important
-software is now closing (followed by a new line)
-*/
-
 const readline = require('readline');
 
-const cmd = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-cmd.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}!`);
-  cmd.on('close', () => {
-    console.log('This important software is now closing');
-  });
-  cmd.close();
+// Display initial prompt
+console.log('Welcome to Holberton School, what is your name?');
+
+// Read user input
+rl.on('line', (input) => {
+  if (input.trim() === '') {
+    rl.close(); // Close the interface if input is empty
+  } else {
+    // Display user's name
+    console.log(`Your name is: ${input}`);
+    rl.close();
+  }
 });
+
+// Handle program termination
+if (!process.stdin.isTTY) {
+  rl.on('close', () => {
+    console.log('This important software is now closing');
+    process.exit(0);
+  });
+}
