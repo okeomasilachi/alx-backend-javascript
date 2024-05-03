2d289544d5e4bfb9245c6dc0ea89854cbd7653b5
okeomasilchi <okeomasilachi@gmail.com>
2024-05-03 14:58:45 +0100

    Refactor API files and tests for the payment system

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -5,7 +5,7 @@ const port = 7865;
 const validateIdParam = (req, res, next) => {
   const id = req.params.id;
   if (!/^\d+$/.test(id)) {
-    return res.status(404).send('Invalid ID. ID must be a number.');
+    return res.status(404).send('id: Must be a number.');
   }
   next();
 };
@@ -16,7 +16,8 @@ app.get('/', (req, res) => {
 
 app.get('/cart/:id', validateIdParam, (req, res) => {
   const cartId = req.params.id;
-  res.send(`Payment methods for cart ${cartId}`);
+
+  res.send(`Payment methods for cart :${cartId}`);
 });
 
 const server = app.listen(port, () => {
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -11,7 +11,8 @@ describe('Index Page', () => {
   });
 
   // Test for correct result
-  it('should return "Welcome to the payment system"', (done) => {
+  it('should return "Welcome to the payment system"',
+  (done) => {
     request('http://localhost:7865', (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
@@ -19,11 +20,67 @@ describe('Index Page', () => {
   });
 
   // Additional test (optional)
-  it('should handle unknown routes with 404 status', (done) => {
+  it('should handle unknown routes with 404 status',
+  (done) => {
     request('http://localhost:7865/nonexistent',
       (error, response, body) => {
         expect(response.statusCode).to.equal(404);
         done();
       });
+  });
+
+  it('should return status code 200 when :id is a number',
+  (done) => {
+    request('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is not a number',
+  (done) => {
+    request('http://localhost:7865/cart/abc', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('should return a string with the id when :id is a number',
+  (done) => {
+    request('http://localhost:7865/cart/2345', (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.be.a('string');
+      expect(body).to.contain(
+        'Payment methods for cart :2345'
+      );
+      done();
+    });
+  });
+
+  it('should return 404 and error message when :id is NOT a number',
+  (done) => {
+    request('http://localhost:7865/cart/abc', (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(404);
+      expect(body).to.be.a('string');
+      expect(body).to.contain('id: Must be a number.');
+
+      done();
+    });
+  });
+
+  it('should return status code 404 when :id is missing', (done) => {
+    request('http://localhost:7865/cart/', (error, response, body) => {
+      if (error) {
+        return done(error);
+      }
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
   });
 });
