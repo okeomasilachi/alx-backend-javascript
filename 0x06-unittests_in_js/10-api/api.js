bb3f00f9165db3fa5cd532be2afe3d0141344676
okeomasilchi <okeomasilachi@gmail.com>
2024-05-03 15:30:00 +0100

    Update chai version to 4.2.0 in package-lock.json

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 7865;
+
+const validateIdParam = (req, res, next) => {
+  const id = req.params.id;
+  if (!/^\d+$/.test(id)) {
+    return res.status(404).send('id: Must be a number.');
+  }
+  next();
+};
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', validateIdParam, (req, res) => {
+  const cartId = req.params.id;
+
+  res.send(`Payment methods for cart :${cartId}`);
+});
+
+const server = app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = server;
